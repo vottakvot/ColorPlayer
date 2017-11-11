25a3a51336ca6ac8a6107c1a483c73f03c740180
user <cvc575@gmail.com>
2017-11-12 00:41:26 +0300

    Refactoring. Add behavior for control panel.

--- Player/src/main/java/ru/testsimpleapps/coloraudioplayer/ui/adapters/BaseListenerEndlessAdapter.java
+++ Player/src/main/java/ru/testsimpleapps/coloraudioplayer/ui/adapters/BaseListenerEndlessAdapter.java
@@ -16,17 +16,19 @@ public abstract class BaseListenerEndlessAdapter extends RecyclerView.OnScrollLi
 
     @Override
     public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-        LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
-        final int visibleItemCount = layoutManager.getChildCount();
-        final int totalItemCount = layoutManager.getItemCount();
-        final int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
         if (newState == RecyclerView.SCROLL_STATE_SETTLING) {
+            final LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+            final int visibleItemCount = layoutManager.getChildCount();
+            final int totalItemCount = layoutManager.getItemCount();
+            final int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+
             if (loadingMode) {
                 if (totalItemCount > previousTotalItemCount) {
                     loadingMode = false;
                     previousTotalItemCount = totalItemCount;
                 }
             }
+
             if (!loadingMode && visibleItemCount + firstVisibleItemPosition >= totalItemCount - VISIBLE_THRESHOLD) {
                 loadingMode = true;
                 onLoadMore();
